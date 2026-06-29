5b52e567f6a217683de0beeb5fd6be4df100814d
Aditya Patil <aditya.patil@aticanlabs.com>
2026-06-29 23:37:25 +0530

    prisma init

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-import express from "express"
-import dotenv from "dotenv"
-dotenv.config();
-
-const app = express()
-app.use(express.json());
-
-//routes
-
-const PORT = process.env.PORT;
-app.listen(PORT,()=>console.log(`Listening on port ${PORT}`));
+console.log("hi")
